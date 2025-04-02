35997615d7de2a0443031588d58589661ae8b382
Ange5051 <147865401+Ange5051@users.noreply.github.com>
2025-04-02 23:13:10 +0300

    Add files via upload

---
+++ prac 30.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P30_1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                List<Person> people = new List<Person>();
+                people.Add(new Russian("Александр"));
+                people.Add(new American("Alex"));
+                people.Add(new Ukraine("Олександр"));
+                foreach (Person person in people)
+                {
+                    person.SayHello();
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
